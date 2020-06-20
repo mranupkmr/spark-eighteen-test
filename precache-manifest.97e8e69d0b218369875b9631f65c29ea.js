6585ecd5033694f1274f57c6c546f004914cb61f
mranupkmr <mr.anupkmr@gmail.com>
2020-06-20 15:27:17 +0530

    Updates

---
+++ precache-manifest.97e8e69d0b218369875b9631f65c29ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fef0402527f5cace10f382a8589aad",
+    "url": "/mranupkmr/spark-eighteen-test/index.html"
+  },
+  {
+    "revision": "6e7d4ea78e5236b83c5e",
+    "url": "/mranupkmr/spark-eighteen-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e537565f489f0b504c9",
+    "url": "/mranupkmr/spark-eighteen-test/static/css/main.8694f905.chunk.css"
+  },
+  {
+    "revision": "6e7d4ea78e5236b83c5e",
+    "url": "/mranupkmr/spark-eighteen-test/static/js/2.00c89254.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mranupkmr/spark-eighteen-test/static/js/2.00c89254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e537565f489f0b504c9",
+    "url": "/mranupkmr/spark-eighteen-test/static/js/main.18887bef.chunk.js"
+  },
+  {
+    "revision": "6693330a5ba8f7c4454e",
+    "url": "/mranupkmr/spark-eighteen-test/static/js/runtime-main.f75a54f8.js"
+  }
+]);
